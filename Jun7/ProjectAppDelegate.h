e88fbc497eddcdd1586e30611b1891435697c749
Lino Guglielmo <linogug@aol.com>
2012-06-05 21:22:04 -0400

    Initial Commit

---
+++ Jun7/ProjectAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ProjectAppDelegate.h
+//  Jun7
+//
+//  Created by Lino Guglielmo on 6/5/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ProjectAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Jun7/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Jun7
+//
+//  Created by Lino Guglielmo on 6/5/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ProjectAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([ProjectAppDelegate class]));
+	}
+}
